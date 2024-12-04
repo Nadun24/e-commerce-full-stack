fa5000d7a44f6f85d36cd89ada4e989f5c5fec00
Nadun Sameera Bandara <bandarans2000@gmail.com>
2024-12-04 07:13:34 +0530

    change user table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->tinyInteger('role_as')->default(1)->comment('1 - Admin, 2 - User, 3 - Manager');
             $table->timestamps();
         });
 
